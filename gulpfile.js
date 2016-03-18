f4e81a32b23cae2aff96c679dca69677d5ea8aa7
sunhailin <sunhailin215@163.com>
2016-03-18 17:06:32 +0800

    zhihu

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp=require("gulp");
 var less=require("gulp-less");
 var autoPreFixer=require("gulp-autoprefixer");
+var gls=require("gulp-live-server");
 
 var lessAll="dome/zhihu/css/less/*.less";
 var lessPath="dome/zhihu/css/less/zhihu.less";
 var outCssPath="dome/zhihu/css";
+var htmlPath="dome/zhihu/";
+
+gulp.task("server", function () {
+    var server=gls.static("/");
+    server.start();
+    gulp.watch([outCssPath+"/*.css",htmlPath+"/*.html"],function(file){
+        server.notify.apply(server,[file]);
+    });
+});
 
 gulp.task("less",function(){
     gulp.src(lessPath)
@@ -14,9 +24,10 @@ gulp.task("less",function(){
             remove:true
         }))
         .pipe(gulp.dest(outCssPath));
-
 });
 
 gulp.task("watchLess",function(){
     gulp.watch(lessAll,["less"]);
 });
+
+gulp.task("default",["server","watchLess"]);
