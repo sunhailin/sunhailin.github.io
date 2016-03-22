7c80509ae17a3c89679e695b824f467fcd5e07fb
sunhailin <sunhailin215@163.com>
2016-03-22 14:55:21 +0800

    dome

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,60 @@ var less=require("gulp-less");
 var autoPreFixer=require("gulp-autoprefixer");
 var gls=require("gulp-live-server");
 
-var lessAll="dome/zhihu/css/less/*.less";
-var lessPath="dome/zhihu/css/less/zhihu.less";
-var outCssPath="dome/zhihu/css";
-var htmlPath="dome/zhihu/";
+//var zhihuLessAll="dome/zhihu/css/less/*.less";
+//var zhihuLessPath="dome/zhihu/css/less/zhihu.less";
+//var zhihuOutCssPath="dome/zhihu/css";
+//var zhihuHtmlPath="dome/zhihu/";
+//
+//gulp.task("zhihuServer", function () {
+//    var server=gls.static("/");
+//    server.start();
+//    gulp.watch([zhihuOutCssPath+"/*.css",zhihuHtmlPath+"/*.html"],function(file){
+//        server.notify.apply(server,[file]);
+//    });
+//});
+//
+//gulp.task("zhihuLess",function(){
+//    gulp.src(zhihuLessPath)
+//        .pipe(less())
+//        .pipe(autoPreFixer({
+//            browsers:["last 2 versions"],
+//            remove:true
+//        }))
+//        .pipe(gulp.dest(zhihuOutCssPath));
+//});
+//
+//gulp.task("zhihuWatchLess",function(){
+//    gulp.watch(zhihuLessAll,["zhihuLess"]);
+//});
 
-gulp.task("server", function () {
+//gulp.task("default",["zhihuServer","zhihuWatchLess"]);
+
+var domeLessAll="dome/css/less/*.less";
+var domeLessPath="dome/css/less/dome.less";
+var domeOutCssPath="dome/css";
+var domeHtmlPath="dome/index.html";
+
+gulp.task("domeServer", function () {
     var server=gls.static("/");
     server.start();
-    gulp.watch([outCssPath+"/*.css",htmlPath+"/*.html"],function(file){
+    gulp.watch([domeOutCssPath+"/*.css",domeHtmlPath],function(file){
         server.notify.apply(server,[file]);
     });
 });
 
-gulp.task("less",function(){
-    gulp.src(lessPath)
+gulp.task("domeLess",function(){
+    gulp.src(domeLessPath)
         .pipe(less())
         .pipe(autoPreFixer({
             browsers:["last 2 versions"],
             remove:true
         }))
-        .pipe(gulp.dest(outCssPath));
+        .pipe(gulp.dest(domeOutCssPath));
 });
 
-gulp.task("watchLess",function(){
-    gulp.watch(lessAll,["less"]);
+gulp.task("domeWatchLess",function(){
+    gulp.watch(domeLessAll,["domeLess"]);
 });
 
-gulp.task("default",["server","watchLess"]);
+gulp.task("default",["domeServer","domeWatchLess"]);
